83dfed6a65fd0ea31fe6b9fa649d51f05b1bcea4
PedroRocha-a22003766 <pedro.mgmrocha@gmail.com>
2022-10-11 21:05:02 +0100

    alteracoes

--- tfc/urls.py
+++ tfc/urls.py
@@ -8,5 +8,6 @@ app_name = "tfc"
 
 urlpatterns = [
     path('', views.home_page_view, name='home'),
-    path('home', views.home_page_view, name = 'home')
+    path('home', views.home_page_view, name = 'home'),
+    path('about', views.about_page_view, name = 'about')
 ]
--- tfc/views.py
+++ tfc/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def home_page_view(request):
 	return render(request, 'tfc/home.html')
+
+def about_page_view(request):
+	return render(request, 'tfc/about.html')
