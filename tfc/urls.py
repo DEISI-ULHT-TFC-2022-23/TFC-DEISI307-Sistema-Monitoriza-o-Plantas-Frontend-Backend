558644eda228012eb32c1ce2bd5fffc3c4e87bdc
PedroRocha-a22003766 <pedro.mgmrocha@gmail.com>
2022-10-11 22:00:17 +0100

    alteracoes

--- tfc/urls.py
+++ tfc/urls.py
@@ -3,6 +3,9 @@
 from django.shortcuts import render
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 app_name = "tfc"
 
@@ -11,3 +14,5 @@ urlpatterns = [
     path('home', views.home_page_view, name = 'home'),
     path('about', views.about_page_view, name = 'about')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
