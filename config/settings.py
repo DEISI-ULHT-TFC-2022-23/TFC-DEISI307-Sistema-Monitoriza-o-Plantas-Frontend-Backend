e1a6db3b24c1d55672f931ad62c7d458f8f3e775
PedroRocha-a22003766 <pedro.mgmrocha@gmail.com>
2022-10-11 21:48:04 +0100

    alteracoes

--- config/settings.py
+++ config/settings.py
@@ -110,7 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/tfc/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('tfc/static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
