cbd5a56f4de30435f1d94dce740dede324a27229
PedroRocha-a22003766 <pedro.mgmrocha@gmail.com>
2023-03-26 13:11:57 +0100

    Novas páginas

--- tfc/urls.py
+++ tfc/urls.py
@@ -10,9 +10,15 @@ from django.conf.urls.static import static
 app_name = "tfc"
 
 urlpatterns = [
-    path('', views.home_page_view, name = 'home'),
-    path('home', views.home_page_view, name = 'home'),
-    path('about', views.about_page_view, name = 'about')
-]
+    path('', views.splashscreen_view, name = 'splashscreen'),
+    path('splashscreen', views.splashscreen_view, name = 'splashscreen'),
+    path('login', views.login_view, name = 'login'),
+    path('logout', views.logout_view, name = 'logout'),
+    path('dashboard', views.dashboard_page_view, name = 'dashboard'),
+    path('inicio', views.inicio_page_view, name = 'inicio'),
+    path('plantas', views.plantas_page_view, name = 'plantas'),
+    path('omeujardim', views.jardim_page_view, name = 'omeujardim'),
+    path('definicoes', views.definicoes_page_view, name = 'definicoes'),
+]   
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- tfc/views.py
+++ tfc/views.py
@@ -1,8 +1,48 @@
-from unicodedata import name
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-def home_page_view(request):
-	return render(request, 'tfc/home.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-def about_page_view(request):
-	return render(request, 'tfc/about.html')
+        user = authenticate(
+            request,
+            username = username,
+            password = password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('tfc:dashboard'))
+        else:
+            return render(request, 'tfc/login.html', {
+                'message': 'Credenciais inválidas! Tente Novamente.'
+            })
+
+    return render(request, 'tfc/login.html')
+
+def logout_view(request):
+    logout(request)
+
+    return render(request, 'tfc/login.html', {
+                'message': 'Foi desconectado.'
+            })
+
+def splashscreen_view(request):
+	return render(request, 'tfc/splashscreen.html')
+
+def dashboard_page_view(request):
+	return render(request, 'tfc/dashboard.html')
+
+def inicio_page_view(request):
+	return render(request, 'tfc/inicio.html')
+
+def plantas_page_view(request):
+	return render(request, 'tfc/plantas.html')
+
+def jardim_page_view(request):
+	return render(request, 'tfc/omeujardim.html')
+
+def definicoes_page_view(request):
+      return render(request, 'tfc/definicoes.html')
